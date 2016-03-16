b56c51065c439ec8426b6348abeff674839e9ade
Christoph Wagner <github@pandawhisperer.net>
2016-03-15 23:41:17 -0700

    Updating package.js

--- package.js
+++ package.js
@@ -1,8 +1,17 @@
 Package.describe({
-  summary: "Login service for reddit accounts"
+  name: 'pandawhisperer:reddit-oauth',
+  version: '0.0.8',
+  // Brief, one-line summary of the package.
+  summary: 'Login service for reddit accounts',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/PandaWhisperer/meteor-reddit-oauth.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
 });
 
-Package.on_use(function(api) {
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
   api.use('oauth', ['client', 'server']);
   api.use('oauth2', ['client', 'server']);
   api.use('http', ['client', 'server']);
@@ -11,8 +20,8 @@ Package.on_use(function(api) {
 
   api.export('RedditOauth');
 
-  api.add_files( ['reddit_configure.html', 'reddit_configure.js'], 'client');
+  api.addFiles( ['reddit_configure.html', 'reddit_configure.js'], 'client');
 
-  api.add_files('reddit_server.js', 'server');
-  api.add_files('reddit_client.js', 'client');
+  api.addFiles('reddit_server.js', 'server');
+  api.addFiles('reddit_client.js', 'client');
 });
