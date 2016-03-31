84c8a2099ad334366816c895383d26038dff71dd
Christoph Wagner <github@pandawhisperer.net>
2016-03-31 00:07:37 -0700

    Bumping version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'pandawhisperer:reddit-oauth',
-  version: '0.0.8',
+  version: '0.0.9',
   // Brief, one-line summary of the package.
   summary: 'Login service for reddit accounts',
   // URL to the Git repository containing the source code for this package.
